e0ab2c0e020c32aa6a9e3547797a1060aaf34ae3
Amit Bhoraniya <amit.bhoraniya@infostretch.com>
2020-05-31 17:22:32 +0530

    Adds headspin configuration and code refactored

--- src/test/java/com/infostretch/hs/steps/common/SelectRoomSteps.java
+++ src/test/java/com/infostretch/hs/steps/common/SelectRoomSteps.java
@@ -1,5 +1,7 @@
 package com.infostretch.hs.steps.common;
 
+import static com.qmetry.qaf.automation.ui.webdriver.ElementFactory.$;
+
 import com.infostretch.hs.utils.Constants;
 import com.infostretch.hs.utils.MobileUtils;
 import com.qmetry.qaf.automation.core.ConfigurationManager;
@@ -10,22 +12,39 @@ public class SelectRoomSteps {
 
     @QAFTestStep(description = "capture the room details and click SELECT ROOM options")
     public void captureTheRoomDetailsAndOption() {
-    	
-    	String hotelName = CommonStep.getText("hotel.detail.txt.title");
-    	ConfigurationManager.getBundle().setProperty(Constants.SELECTED_HOTEL,hotelName);
-    	
-    	CommonStep.waitForVisible("hotel.detail.btn.selectroom");
-    	CommonStep.click("hotel.detail.btn.selectroom");
-    	MobileUtils.swipeVertically();
-    
-    	String roomCategoryName = CommonStep.getText("hotel.room.category.detail.txt.name");
-    	String roomCategoryDiscountedPrice = CommonStep.getText("hotel.room.category.detail.txt.discountedprice");
-    	String roomCategoryOriginalPrice = CommonStep.getText("hotel.room.category.detail.txt.originalprice");
-    		
-    	ConfigurationManager.getBundle().setProperty(Constants.SELECTED_ROOM_CATEGORY_NAME,roomCategoryName);
-    	ConfigurationManager.getBundle().setProperty(Constants.SELECTED_ROOM_CATEGORY_DISCOUNTEDPRICE,roomCategoryDiscountedPrice);
-    	ConfigurationManager.getBundle().setProperty(Constants.SELECTED_ROOM_CATEGORY_ORIGINALPRICE,roomCategoryOriginalPrice);
-    
-    	CommonStep.click("hotel.detail.btn.continue");
+
+        String hotelName = CommonStep.getText("hotel.detail.txt.title");
+        ConfigurationManager.getBundle().setProperty(Constants.SELECTED_HOTEL, hotelName);
+
+        CommonStep.waitForVisible("hotel.detail.btn.selectroom");
+
+        CommonStep.click("hotel.detail.btn.selectroom");
+        //check if still present
+        try {
+            $("hotel.detail.btn.selectroom").waitForNotPresent(5000);
+        } catch (Exception e) {
+        }
+        //click retry again
+        if ($("hotel.detail.btn.selectroom").isPresent()) {
+            $("hotel.detail.btn.selectroom").click();
+        }
+
+        MobileUtils.swipeVertically();
+
+        String roomCategoryName = CommonStep.getText("hotel.room.category.detail.txt.name");
+        String roomCategoryDiscountedPrice = CommonStep
+            .getText("hotel.room.category.detail.txt.discountedprice");
+        String roomCategoryOriginalPrice = CommonStep
+            .getText("hotel.room.category.detail.txt.originalprice");
+
+        ConfigurationManager.getBundle()
+            .setProperty(Constants.SELECTED_ROOM_CATEGORY_NAME, roomCategoryName);
+        ConfigurationManager.getBundle()
+            .setProperty(Constants.SELECTED_ROOM_CATEGORY_DISCOUNTEDPRICE,
+                roomCategoryDiscountedPrice);
+        ConfigurationManager.getBundle()
+            .setProperty(Constants.SELECTED_ROOM_CATEGORY_ORIGINALPRICE, roomCategoryOriginalPrice);
+
+        CommonStep.click("hotel.detail.btn.continue");
     }
 }
