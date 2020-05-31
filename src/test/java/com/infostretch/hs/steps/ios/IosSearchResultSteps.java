8c6cece805005a8d919e58570c7c8d0a7fa83adb
nishith.shah <nishith.shah@infostretch.com>
2020-05-31 16:19:54 +0530

    add filter code for ios

--- src/test/java/com/infostretch/hs/steps/ios/IosSearchResultSteps.java
+++ src/test/java/com/infostretch/hs/steps/ios/IosSearchResultSteps.java
@@ -1,11 +1,22 @@
 package com.infostretch.hs.steps.ios;
 
 import com.infostretch.hs.steps.common.SearchResultSteps;
+import com.infostretch.hs.utils.MobileUtils;
+import com.qmetry.qaf.automation.core.ConfigurationManager;
+import com.qmetry.qaf.automation.step.CommonStep;
 import com.qmetry.qaf.automation.step.QAFTestStep;
 
 public class IosSearchResultSteps extends SearchResultSteps {
-	 @QAFTestStep(description = "user filters by price by setting {price} value")
-	    public void applyMinimumPriceFilter(int price) {
-	        // TODO: Due to bug, we cannot change the slider to specific value
-	    }
+	@QAFTestStep(description = "user filters by price by setting {price} value")
+	public void applyMinimumPriceFilter(int price) {
+		// TODO: Due to bug, we cannot change the slider to specific value
+	}
+
+	@QAFTestStep(description = "user scroll and select the {index} item from filter result page")
+	public void userScrollAndSelectHotel(int index) {
+		String loc = String.format(
+				ConfigurationManager.getBundle().getString("search.select.hotel", "search.select.hotel"), index);
+		CommonStep.waitForPresent(loc);
+		MobileUtils.findElementByScrolling(loc, index + 3).click();
+	}
 }
