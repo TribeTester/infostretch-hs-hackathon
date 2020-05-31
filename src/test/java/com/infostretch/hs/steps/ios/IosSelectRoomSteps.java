26c87a64f834a56e5e8728eacbda545cc10f956c
nishith.shah <nishith.shah@infostretch.com>
2020-05-31 17:18:48 +0530

    add room details function

--- src/test/java/com/infostretch/hs/steps/ios/IosSelectRoomSteps.java
+++ src/test/java/com/infostretch/hs/steps/ios/IosSelectRoomSteps.java
@@ -1,7 +1,14 @@
 package com.infostretch.hs.steps.ios;
 
 import com.infostretch.hs.steps.common.SelectRoomSteps;
+import com.qmetry.qaf.automation.step.CommonStep;
+import com.qmetry.qaf.automation.step.QAFTestStep;
 
 public class IosSelectRoomSteps extends SelectRoomSteps {
+	@QAFTestStep(description = "capture the room details and click SELECT ROOM options")
+	public void captureTheRoomDetailsAndOption() {
 
+		CommonStep.click("hotel.detail.btn.selectroom");
+		CommonStep.click("hotel.detail.btn.continue");
+	}
 }
